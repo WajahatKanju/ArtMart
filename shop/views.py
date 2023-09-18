cd044625e03737b911164e96ba008a893c0ade71
WajahatKanju <wajahat5ahmad@gmail.com>
2023-09-18 23:57:30 +0500

    add footer

--- shop/views.py
+++ shop/views.py
@@ -5,5 +5,5 @@ from products.models import Category
 # Create your views here.
 def index(request):
     categories = Category.objects.all()
-    context = {"categories": categories}
+    context = {"categories": categories, "brand_name": "Artmart"}
     return render(request, "shop/index.html", context)
