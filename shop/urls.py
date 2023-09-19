4079963a98de84cfd605121ea8b1ffd78f42719b
WajahatKanju <wajahat5ahmad@gmail.com>
2023-09-19 23:44:04 +0500

    add login/register template

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, register, user_login
 
 urlpatterns = [
     path("", index),
+    path("register/", register, name="register"),
+    path("login/", user_login, name="login"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from products.models import Category
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 
 # Create your views here.
@@ -7,3 +9,27 @@ def index(request):
     categories = Category.objects.all()
     context = {"categories": categories, "brand_name": "Artmart"}
     return render(request, "shop/index.html", context)
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("index")  # Redirect to your desired page after registration
+    else:
+        form = UserCreationForm()
+    return render(request, "authentication/register.html", {"form": form})
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("index")  # Redirect to your desired page after login
+    else:
+        form = AuthenticationForm()
+    return render(request, "authentication/login.html", {"form": form})
