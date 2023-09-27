040c09db79511e62e0a79b0a676edf864436f851
WajahatKanju <wajahat5ahmad@gmail.com>
2023-09-27 11:46:27 +0500

    migrate to cbc

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from .views import index, register, user_login, handler_404
+from .views import handler_404
+from .views import Index, Register, UserLogin, AboutView, ContactView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path("", index),
-    path("register/", register, name="register"),
-    path("login/", user_login, name="login"),
+    path("", Index.as_view(), name="home"),
+    path("register/", Register.as_view(), name="register"),
+    path("login/", UserLogin.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("about/", AboutView.as_view(), name="about"),
+    path("contact/", ContactView.as_view(), name="contact"),
     path("404/", handler_404, name="not_found"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,38 +1,69 @@
 from django.shortcuts import render, redirect
-from products.models import Category
+from django.views import View
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
 from django.contrib.auth import login
 
 
-# Create your views here.
-def index(request):
-    categories = Category.objects.all()
-    context = {"categories": categories, "brand_name": "Artmart"}
-    return render(request, "shop/index.html", context)
+class BaseView(View):
+    site_name = "Artmart"
+    site_mail = "artmartswat@gmail.com"
+
+    def render_template(self, request, template_name, context={}):
+        context["site_name"] = self.site_name
+        context["site_mail"] = self.site_mail
+        return render(request, template_name, context)
 
 
-def register(request):
-    if request.method == "POST":
+class Index(BaseView):
+    def get(self, request):
+        return self.render_template(request, "shop/index.html")
+
+
+class Register(BaseView):
+    def get(self, request):
+        form = UserCreationForm()
+        return self.render_template(
+            request, "authentication/register.html", {"form": form}
+        )
+
+    def post(self, request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect("index")  # Redirect to your desired page after registration
-    else:
-        form = UserCreationForm()
-    return render(request, "authentication/register.html", {"form": form})
+        return self.render_template(
+            request, "authentication/register.html", {"form": form}
+        )
 
 
-def user_login(request):
-    if request.method == "POST":
+class UserLogin(BaseView):
+    def get(self, request):
+        form = AuthenticationForm()
+        return self.render_template(
+            request, "authentication/login.html", {"form": form}
+        )
+
+    def post(self, request):
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
             return redirect("index")  # Redirect to your desired page after login
-    else:
-        form = AuthenticationForm()
-    return render(request, "authentication/login.html", {"form": form})
+        return self.render_template(
+            request, "authentication/login.html", {"form": form}
+        )
+
+
+class AboutView(BaseView):
+    def get(self, request):
+        return self.render_template(request, "shop/about.html")
+
+
+class ContactView(BaseView):
+    def get(self, request):
+        return self.render_template(request, "shop/contact.html")
 
 
 def handler_404(request, *args, **kwargs):
